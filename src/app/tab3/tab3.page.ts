7809161abfed48828c3046f55d4a581052b78c7c
Diane Tobit <diane@acutulus.co>
2019-03-31 11:32:57 -0700

    add pictures

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { UserService } from '../user.service';
+import { Item } from '../../interfaces/_interfaces';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page implements OnInit{
+  postData: Item[] = [];
+
+  constructor(private dataService: DataService, private userService: UserService) {}
+
+  ngOnInit() {
+    this.dataService.getItems()
+    .then(response =>  {
+      console.log(response);
+      this.postData = response.filter(item => {
+        return item.userID === this.userService.user.id;
+      });
+    });
+  }
+
+  doRefresh(event: any) {
+    this.dataService.getItems()
+    .then(response =>  {
+      console.log(response);
+      this.postData = response.filter(item => {
+        return item.userID === this.userService.user.id;
+      });
+    });
+  }
+}
